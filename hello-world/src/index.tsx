54766b12600c76a8fb1c52a0d732e7902d267579
kaji <kajiharalivi@icloud.com>
2022-03-28 22:58:07 +0900

    hora

--- hello-world/src/index.tsx
+++ hello-world/src/index.tsx
@@ -1,33 +1,130 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-interface user{
-  firstName: string;
-  lastName: string;
+// interface User{
+//   firstName: string;
+//   lastName: string;
+// }
+
+// interface Props{
+//   name: String;
+// }
+
+// interface Props2{
+//   date: Date;
+// }
+
+// const user:User ={
+//   firstName: 'ahhaha',
+//   lastName : 'werwerw'
+// };
+
+// function formatName(user: User):string{
+//   return user.firstName + ' ' + user.lastName;
+// }
+
+// function Welcome(props: Props){
+//   return <h1>Hello, {props.name}</h1>
+// }
+
+// function App(){
+//   return(
+//     <div>
+//       <Welcome name="sara" />
+//       <Welcome name="hoge" />
+//       <Welcome name="hora" />
+//     </div>
+//   )
+// }
+
+// function Example(){
+//   const [count,setCount] = useState(0);
+
+//   return(
+//     <div>
+//       <p>You clicked {count} times </p>
+//       <button onClick={() => setCount(count+1)}> Click me </button>
+//     </div>
+//   )
+// }
+
+// function Clock(props: Props2){
+//   return(
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {props.date.toString()}.</h2>
+//     </div>
+//   );
+// }
+
+// //なんでこれでできるんじゃ？？？？？
+// function tick(){
+//   ReactDOM.render(
+//     <App />,
+//     document.getElementById('root')
+//   )
+// }
+
+// setInterval(tick,1000);
+
+// function App(){
+//   return(
+//     <>
+//       {element}
+//       <Clock date={new Date()} />
+//     </>
+//   );
+// }
+
+// const element =(
+//   <h1>
+//     Hello,{formatName(user)}!
+//   </h1>
+// )
+
+// const d :number = 324
+
+// const element = <div>hogehoge</div>
+
+// const element = (
+//   <div>
+//     <h1>Hello!</h1>
+//     <h2>horahorahora</h2>
+//   </div>
+// )
+
+// const element= <Welcome name="sara" />;
+
+const element = <div>hogehoge</div>;
+
+function Clock() {
+  const [date, setDate] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => setDate(new Date()), 1000);
+  }, []);
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {date.toString()}.</h2>
+    </div>
+  );
 }
 
-const user:user ={
-  firstName: 'ahhaha',
-  lastName : 'werwerw'
-};
-
-function formatName(user: user):string{
-  return user.firstName + ' ' + user.lastName;
+function App(): JSX.Element {
+  const now = new Date();
+  return (
+    <>
+      {element}
+      {now.toString()}
+      <Clock />
+    </>
+  );
 }
-
-
-const element =(
-  <h1>
-    Hello,{formatName(user)}!
-  </h1>
-)
-
 
 ReactDOM.render(
-  element,
+  <Clock />,
   document.getElementById('root')
 );
 
